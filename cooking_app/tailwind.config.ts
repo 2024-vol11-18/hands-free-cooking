530196d03cc6a786877864e22f4bc7411014d76e
suisan0731 <suisan0731@gmail.com>
2024-07-31 04:06:41 +0900

    change branch

--- cooking_app/tailwind.config.ts
+++ cooking_app/tailwind.config.ts
@@ -9,32 +9,12 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     './node_modules/preline/preline.js',
   ],
-  safelist: [{ pattern: /^border-/ }, { pattern: /^bg-/ }, { pattern: /^text-/ }],
   theme: {
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-      spacing: {
-        '8xl': '96rem',
-        '9xl': '128rem',
-      },
-      borderRadius: {
-        '4xl': '2rem',
-      },
-      colors: {
-        primary: colors.blue,
-        secondary: colors.slate,
-        green: colors.green,
-        red: colors.red,
-        blue: colors.blue,
-        gray: colors.gray,
-      },
-      fontFamily: {
-        sans: ['Graphik', 'sans-serif'],
-        serif: ['Merriweather', 'serif'],
       },
     },
   },
