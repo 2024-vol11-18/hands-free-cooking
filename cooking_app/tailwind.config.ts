75e7d71bd144f1b870a54caf98908fc4c8e23535
suisan0731 <suisan0731@gmail.com>
2024-07-31 03:47:02 +0900

    フロントの環境を整えた

--- cooking_app/tailwind.config.ts
+++ cooking_app/tailwind.config.ts
@@ -1,11 +1,15 @@
 import type { Config } from "tailwindcss";
+import colors from 'tailwindcss/colors';
+
 
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './node_modules/preline/preline.js',
   ],
+  safelist: [{ pattern: /^border-/ }, { pattern: /^bg-/ }, { pattern: /^text-/ }],
   theme: {
     extend: {
       backgroundImage: {
@@ -13,8 +17,29 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      spacing: {
+        '8xl': '96rem',
+        '9xl': '128rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      },
+      colors: {
+        primary: colors.blue,
+        secondary: colors.slate,
+        green: colors.green,
+        red: colors.red,
+        blue: colors.blue,
+        gray: colors.gray,
+      },
+      fontFamily: {
+        sans: ['Graphik', 'sans-serif'],
+        serif: ['Merriweather', 'serif'],
+      },
     },
   },
-  plugins: [],
+  plugins: [
+    require('preline/plugin'),
+  ],
 };
 export default config;
