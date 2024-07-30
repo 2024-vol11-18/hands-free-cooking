4311dca8361995b8b984f9d44b7de05c0af49448
suisan0731 <suisan0731@gmail.com>
2024-07-30 21:05:20 +0900

    フロントのフォルダを構成

---
+++ cooking_app/src/app/cooking/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import Image from "next/image";
+import useSWR from "swr";
+import useSWRMutation from "swr/mutation";
+
+export default function Cooking() {
+    return (
+    <>
+        <p>here is /cooking page</p>
+    </>
+    )
+}
---
+++ cooking_app/src/app/recipe/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import Image from "next/image";
+import useSWR from "swr";
+import useSWRMutation from "swr/mutation";
+
+export default function Recipe() {
+    return (
+    <>
+        <p>here is /recipe page</p>
+    </>
+    )
+}
