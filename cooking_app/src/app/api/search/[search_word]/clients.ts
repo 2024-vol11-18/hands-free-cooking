8dc0f6ec8607bebfb3e6e6449eb69690fdcaec46
naosuke884 <884naoki.general@gmail.com>
2024-08-02 05:13:08 +0900

    4分から2分になった

--- cooking_app/src/app/api/search/[search_word]/clients.ts
+++ cooking_app/src/app/api/search/[search_word]/clients.ts
@@ -17,6 +17,7 @@ export class OpenAISearchClient implements ISearchClient{
 
 export class RakutenRecipeScraipingSearchClient implements ISearchClient{
     base_url: string = "https://recipe.rakuten.co.jp/search/"
+    recipe_url: string = "https://recipe.rakuten.co.jp/recipe/"
 
     async search(query: string): Promise<SearchGetResponseType> {
         const res = await fetch(this.base_url + query)
@@ -39,19 +40,85 @@ export class RakutenRecipeScraipingSearchClient implements ISearchClient{
             })
         })
         const ids = Array.from(ids_)
-        const settled_res = await Promise.allSettled(ids.map(async (id)=>{return getService().get(id)}))
-        const recipes: (RecipesType | null)[] = await Promise.all(settled_res.map(async (res)=>{
-            if(res.status === "rejected"){return null }
-            const recipe = res.value
-            return {
-                id: recipe.id,
-                title: recipe.title,
-                time: recipe.time,
-                cost: recipe.cost,
-                comment: recipe.comment
-            }
+        // const res = await fetch(this.base_url + id)
+        // const virtualConsole = new jsdom.VirtualConsole();
+        // virtualConsole.on("error", () => {});
+        // const dom = new JSDOM(await res.text(),{virtualConsole})
+
+        // const title_ = dom.window.document.querySelector(".page_title__text")
+        // if(title_ === null){throw new Error("title not found")}
+        // const title = title_.textContent
+        // if(title === null){throw new Error("title not found")}
+
+        // const time_ = dom.window.document.querySelector(".recipe_info__time")
+        // if(time_ === null){throw new Error("time not found")}
+        // let time = time_.textContent
+        // if(time === null){throw new Error("time not found")}
+        // time = deleteSpaceAndNewLine(time)
+
+        // const cost_ = dom.window.document.querySelector(".recipe_info__cost")
+        // if(cost_ === null){throw new Error("cost not found")}
+        // let cost = cost_.textContent
+        // if(cost === null){throw new Error("cost not found")}
+        // cost = deleteSpaceAndNewLine(cost)
+
+
+        // const comment_ = dom.window.document.querySelector(".recipe_info_user__comment")
+        // if(comment_ === null){throw new Error("comment not found")}
+        // const comment = comment_.textContent
+        // if(comment === null){throw new Error("comment not found")}
+
+        // const materials_ = dom.window.document.querySelectorAll(".recipe_material__list > li")
+        // if(materials_ === null){throw new Error("materials not found")}
+        const settled_res = await Promise.allSettled(ids.map((id)=>{
+            return fetch(this.recipe_url + id)
+            .then((res)=>{
+                return res.text()
+            })
+            .then((text)=>{
+                const dom = new JSDOM(text,{virtualConsole})
+
+                const title_ = dom.window.document.querySelector(".page_title__text")
+                if(title_ === null){throw new Error("title not found")}
+                const title = title_.textContent
+                if(title === null){throw new Error("title not found")}
+
+                const time_ = dom.window.document.querySelector(".recipe_info__time")
+                if(time_ === null){throw new Error("time not found")}
+                let time = time_.textContent
+                if(time === null){throw new Error("time not found")}
+                time = deleteSpaceAndNewLine(time)
+
+                const cost_ = dom.window.document.querySelector(".recipe_info__cost")
+                if(cost_ === null){throw new Error("cost not found")}
+                let cost = cost_.textContent
+                if(cost === null){throw new Error("cost not found")}
+                cost = deleteSpaceAndNewLine(cost)
+
+
+                const comment_ = dom.window.document.querySelector(".recipe_info_user__comment")
+                if(comment_ === null){throw new Error("comment not found")}
+                const comment = comment_.textContent
+                if(comment === null){throw new Error("comment not found")}
+
+                return {
+                    id: id,
+                    title: title,
+                    time: time,
+                    cost: cost,
+                    comment: comment
+                }
+            })
         }))
+        const recipes: (RecipesType | null)[] = settled_res.map((res)=>{
+            if(res.status === "rejected"){return null }
+            return res.value
+        })
         
         return { recipes: recipes.filter((recipe)=>recipe !== null) }
     }
+}
+
+function deleteSpaceAndNewLine(str: string): string{
+    return str.replace(/\s+/g, "").replace(/\n/g, "")
 }
