024b73f05e18a67afe1deca41389a305d1386e36
naosuke884 <884naoki.general@gmail.com>
2024-08-02 17:37:01 +0900

    削除してたモックをコメントアウトして戻した

--- cooking_app/src/app/api/search/[search_word]/route.ts
+++ cooking_app/src/app/api/search/[search_word]/route.ts
@@ -4,5 +4,18 @@ import { getService } from './controller';
 export async function GET(request: NextRequest, { params }: { params: { search_word: string } }): Promise<NextResponse> {
     const service = getService()
 
+    //  //mock
+    //  const mock_res: SearchGetResponseType = {
+    //     recipes: [
+    //       {
+    //         id: 1040023642,
+    //         title: "冬瓜と豚ばら肉の煮物",
+    //         time: "約30分",
+    //         cost: "300円前後",
+    //         comment: "しっかり味が染みご飯にあいます♪"
+    //       },
+    //     ]
+    //   }
+    // return NextResponse.json(mock_res)
     return NextResponse.json(await service.search(params.search_word))
 }
