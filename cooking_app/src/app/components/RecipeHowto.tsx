57a05f9cbe4c9eb2b162584c734cf7f8e1bbf6b8
hareiharei <harei.whitewings@gmail.com>
2024-08-03 13:58:11 +0900

    hoetoSizeを追加。レシピの現在の工程の区間の色を指定

--- cooking_app/src/app/components/RecipeHowto.tsx
+++ cooking_app/src/app/components/RecipeHowto.tsx
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react';
 
 const RecipeHowto = ({
+    howtoSize,
     order,
     text,
     handlePreviousStep,
     handleNextStep
 }: { 
+    howtoSize: number,
     order: number, 
     text: string,
     handlePreviousStep: any,
@@ -14,17 +16,17 @@ const RecipeHowto = ({
 
     return (
         <div className="relative flex flex-col items-center mt-4">
-            <span className="inline-flex items-center gap-x-1.5 py-1.5 px-4 rounded-full text-lg font-medium bg-gray-100 text-gray-800 dark:bg-white/10 dark:text-white">
+            <span className="inline-flex items-center gap-x-1.5 py-1.5 px-4 rounded-full text-lg font-medium bg-xanthous text-princetonorange">
                 {order}
             </span>
-            <p className="text-lg mx-8 my-4">
+            <p className="text-lg mx-8 my-4 text-smokyblack">
                 {text}
             </p>
             <div className="absolute bottom-0 mb-4 flex">
-                <button onClick={handlePreviousStep} type="button" className="mx-6 py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-full border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 focus:outline-none focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-800 dark:focus:bg-neutral-800">
+                <button onClick={handlePreviousStep} type="button" className="mx-6 py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-full border border-babypowder bg-selectiveyellow text-babypowder shadow-sm hover:bg-princetonorange focus:outline-none focus:bg-princetonorange disabled:opacity-50 disabled:pointer-events-none">
                 前の工程へ
                 </button>
-                <button onClick={handleNextStep} type="button" className="mx-6 py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-full border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 focus:outline-none focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-800 dark:focus:bg-neutral-800">
+                <button onClick={handleNextStep} type="button" className="mx-6 py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-full border border-babypowder bg-selectiveyellow text-babypowder shadow-sm hover:bg-princetonorange focus:outline-none focus:princetonorange disabled:opacity-50 disabled:pointer-events-none">
                 次の工程へ
                 </button>
             </div>
