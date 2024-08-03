ec154302b57e1683425e297d62eeaaa91af6a024
hareiharei <harei.whitewings@gmail.com>
2024-08-03 15:02:12 +0900

    材料の表の位置や幅を調整

--- cooking_app/src/app/components/RecipeMaterials.tsx
+++ cooking_app/src/app/components/RecipeMaterials.tsx
@@ -9,12 +9,12 @@ const RecipeMaterials = ({ materials }: {
 }) => {
     return (
         <div className="w-full">
-            <div className="flex flex-col max-h-[250px] overflow-y-auto overflow-x-hidden [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-track]:bg-gray-100 [&::-webkit-scrollbar-thumb]:bg-gray-300 dark:[&::-webkit-scrollbar-track]:bg-neutral-700">
-                <div className="-m-1.5">
-                    <div className="p-1.5 min-w-full inline-block align-middle">
+            <div className="flex flex-col max-h-[270px] overflow-y-auto overflow-x-hidden [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-track]:bg-xanthous [&::-webkit-scrollbar-thumb]:bg-xanthoussaturated">
+                <div className="m-5">
+                    <div className="p-2 min-w-full inline-block align-middle rounded-xl bg-babypowder">
                         <div className="overflow-hidden">
-                            <table className="w-full min-w-full divide-y divide-gray-200">
-                                <tbody className="divide-y divide-gray-200">
+                            <table className="w-full min-w-full divide-y devide-princetonorangej">
+                                <tbody className="divide-y divide-princetonorange">
                                     {materials?.map((material: { item: string, serving: string }, index: number) => (
                                         <tr key={index}>
                                             <td className="px-6 py-2 whitespace-normal break-words text-sm font-medium text-smokyblack">
