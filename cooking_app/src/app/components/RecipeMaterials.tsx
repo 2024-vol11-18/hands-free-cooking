477dc5a6e7504df939420426652059e6fd9fdddd
hareiharei <harei.whitewings@gmail.com>
2024-08-03 11:20:47 +0900

    材料の区間にスクロールバーを追加

--- cooking_app/src/app/components/RecipeMaterials.tsx
+++ cooking_app/src/app/components/RecipeMaterials.tsx
@@ -9,8 +9,8 @@ const RecipeMaterials = ({ materials }: {
 }) => {
     return (
         <div>
-            <div className="flex flex-col h-full w-full overflow-y-auto">
-                <div className="-m-1.5 overflow-x-auto h-full">
+            <div className="flex flex-col max-h-[250px] overflow-y-auto [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-track]:bg-gray-100 [&::-webkit-scrollbar-thumb]:bg-gray-300 dark:[&::-webkit-scrollbar-track]:bg-neutral-700 dark:[&::-webkit-scrollbar-thumb]:bg-neutral-500">
+                <div className="-m-1.5">
                     <div className="p-1.5 min-w-full inline-block align-middle">
                         <div className="overflow-hidden">
                             <table className="min-w-full divide-y divide-gray-200 dark:divide-neutral-700">
