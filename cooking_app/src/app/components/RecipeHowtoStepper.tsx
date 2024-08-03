08d1e15e4335e9011f8e637c4861f9cd9eb9b7e9
hareiharei <harei.whitewings@gmail.com>
2024-08-03 22:11:06 +0900

    属性をcamelCaseに直した

--- cooking_app/src/app/components/RecipeHowtoStepper.tsx
+++ cooking_app/src/app/components/RecipeHowtoStepper.tsx
@@ -16,7 +16,7 @@ const RecipeHowtoStepper = ({
         <div dataHsStepper={`{ "currentIndex": ${order} }`}>
             <ul className="relative flex flex-row gap-x-2">
               {completedOrders.map((completedOrder, index) => (
-                <li className="flex items-center gap-x-2 shrink basis-0 flex-1 group success" data-hs-stepper-nav-item={`{ "index": ${index}, "isCompleted": true }`}>
+                <li className="flex items-center gap-x-2 shrink basis-0 flex-1 group success" dataHsStepperNavItem={`{ "index": ${index}, "isCompleted": true }`}>
                   <span className="min-w-7 min-h-7 group inline-flex items-center text-xs align-middle">
                     <span className="size-7 flex justify-center items-center shrink-0 bg-peach font-medium text-babypowder rounded-full hs-stepper-active:bg-xanthoussaturated hs-stepper-active:text-babypowder hs-stepper-success:bg-xanthous hs-stepper-success:text-babypowder hs-stepper-completed:bg-xanthous">
                       <span className="hs-stepper-success:hidden hs-stepper-completed:hidden">{completedOrder}</span>
@@ -30,7 +30,7 @@ const RecipeHowtoStepper = ({
               ))}
               
           
-              <li className="flex items-center gap-x-2 shrink basis-0 flex-1 group active" data-hs-stepper-nav-item={`{ "index": ${order} }`}>
+              <li className="flex items-center gap-x-2 shrink basis-0 flex-1 group active" dataHsStepperNavItem={`{ "index": ${order} }`}>
                 <span className="min-w-7 min-h-7 group inline-flex items-center text-xs align-middle">
                   <span className="size-7 flex justify-center items-center shrink-0 bg-peach font-medium text-babypowder rounded-full hs-stepper-active:bg-xanthoussaturated hs-stepper-active:text-babypowder hs-stepper-success:bg-xanthous hs-stepper-success:text-babypowder hs-stepper-completed:bg-xanthous">
                     <span className="hs-stepper-success:hidden hs-stepper-completed:hidden">{order}</span>
@@ -42,7 +42,7 @@ const RecipeHowtoStepper = ({
                 <div className="w-full h-px flex-1 bg-gray-200 group-last:hidden hs-stepper-success:bg-xanthous hs-stepper-completed:bg-xanthous dark:bg-neutral-700 dark:hs-stepper-success:bg-xanthous dark:hs-stepper-completed:bg-teal-600"></div>
               </li>
               {unfinishedOrders.map((unfinishedOrder, index) => (
-                <li className="flex items-center gap-x-2 shrink basis-0 flex-1 group" data-hs-stepper-nav-item={`{ "index": ${index + order} }`}>
+                <li className="flex items-center gap-x-2 shrink basis-0 flex-1 group" dataHsStepperNavItem={`{ "index": ${index + order} }`}>
                   <span className="min-w-7 min-h-7 group inline-flex items-center text-xs align-middle">
                     <span className="size-7 flex justify-center items-center shrink-0 bg-peach font-medium text-babypowder rounded-full group-focus:bg-gray-200 hs-stepper-active:bg-xanthoussaturated hs-stepper-active:text-babypowder hs-stepper-success:bg-xanthous hs-stepper-success:text-babypowder hs-stepper-completed:bg-xanthous">
                       <span className="hs-stepper-success:hidden hs-stepper-completed:hidden">{unfinishedOrder}</span>
