70d551a9b94555d7e13c7f4fb6b730219736207c
hareiharei <harei.whitewings@gmail.com>
2024-08-03 14:34:20 +0900

    材料の表のテキストが改行され、cooking画面が画面の幅と同じになるようにした

--- cooking_app/src/app/components/RecipeMaterials.tsx
+++ cooking_app/src/app/components/RecipeMaterials.tsx
@@ -8,19 +8,19 @@ const RecipeMaterials = ({ materials }: {
     }>
 }) => {
     return (
-        <div>
-            <div className="flex flex-col max-h-[250px] overflow-y-auto [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-track]:bg-gray-100 [&::-webkit-scrollbar-thumb]:bg-gray-300 dark:[&::-webkit-scrollbar-track]:bg-neutral-700 dark:[&::-webkit-scrollbar-thumb]:bg-neutral-500">
+        <div className="w-full">
+            <div className="flex flex-col max-h-[250px] overflow-y-auto [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-track]:bg-gray-100 [&::-webkit-scrollbar-thumb]:bg-gray-300 dark:[&::-webkit-scrollbar-track]:bg-neutral-700">
                 <div className="-m-1.5">
                     <div className="p-1.5 min-w-full inline-block align-middle">
                         <div className="overflow-hidden">
-                            <table className="min-w-full divide-y divide-gray-200 dark:divide-neutral-700">
-                                <tbody className="divide-y divide-gray-200 dark:divide-neutral-700">
+                            <table className="w-full min-w-full divide-y divide-gray-200">
+                                <tbody className="divide-y divide-gray-200">
                                     {materials?.map((material: { item: string, serving: string }, index: number) => (
                                         <tr key={index}>
-                                            <td className="px-6 py-2 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-neutral-200">
+                                            <td className="px-6 py-2 whitespace-normal break-words text-sm font-medium text-smokyblack">
                                                 {material.item}
                                             </td>
-                                            <td className="px-6 py-2 whitespace-nowrap text-sm text-gray-800 dark:text-neutral-200">
+                                            <td className="px-6 py-2 whitespace-normal break-words text-sm text-smokyblack">
                                                 {material.serving}
                                             </td>
                                         </tr>
